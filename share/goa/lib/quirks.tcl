2c604b84a0b4ccfc8414ca065699397dcb0a978c
Norman Feske <norman.feske@genode-labs.com>
2020-03-08 17:58:38 +0100

    Support for using libSDL

--- share/goa/lib/quirks.tcl
+++ share/goa/lib/quirks.tcl
@@ -19,3 +19,20 @@ if {[using_api stdcxx]} {
 	lappend include_dirs [file join $stdcxx_include_dir stdcxx std]
 	lappend include_dirs [file join $stdcxx_include_dir stdcxx c_global]
 }
+
+if {[using_api sdl]} {
+
+	# CMake's detection of libSDL expects the library named uppercase
+	set symlink_name [file join $abi_dir SDL.lib.so]
+	if {![file exists $symlink_name]} {
+		file link -symbolic $symlink_name "sdl.lib.so" }
+
+	# search for headers in the inlude/SDL sub directory
+	set sdl_include_dir [file join [api_archive_dir sdl] include SDL]
+	lappend include_dirs $sdl_include_dir
+
+	# bring CMake on the right track to find the headers and library
+	lappend cmake_quirk_args "-DSDL_INCLUDE_DIR=$sdl_include_dir"
+	lappend cmake_quirk_args "-DCMAKE_SYSTEM_LIBRARY_PATH='$abi_dir'"
+	lappend cmake_quirk_args "-DSDL_LIBRARY:STRING=':sdl.lib.so'"
+}
