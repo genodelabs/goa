74afd510d89ac612c713bbe0b6c94d417491cc20
Johannes Schlatow <johannes.schlatow@genode-labs.com>
2022-09-28 14:16:14 +0200

    add Vivado support
    This commit adds support for recreating Xilinx Vivado projects and generating
    bitstreams. For this purpose, Goa detects the presence of a vivado.tcl
    file in the src/ directory. This file must be created in Vivado with the
    following command:
    
    write_project_tcl -paths_relative_to /path/to/workspace/dir vivado.tcl
    
    Furthermore, the required source files (as referenced in the
    checkRequiredFiles procedure in the tcl file) must be manually copied into
    the src/ directory.
    
    Note that the Vivado tools must have been installed and sourced.

---
+++ share/goa/vivado/generate_bitstream.tcl
@@ -0,0 +1,63 @@
+proc findProject { } {
+	set files [glob -nocomplain [file join * *.xpr]]
+	foreach file $files { return $file }
+
+	puts "ERROR: Could not find project file (.xpr) in build directory '[pwd]'\n"
+	return ""
+}
+
+set target "fpga.bit"
+
+set jobs "1"
+if { $::argc > 0 } {
+	for {set i 0} {$i < $::argc} {incr i} {
+		set option [string trim [lindex $::argv $i]]
+		switch -regexp -- $option {
+			"--jobs"         { incr i; set jobs   [lindex $::argv $i] }
+			"--target"       { incr i; set target [lindex $::argv $i] }
+		default {
+			if { [regexp {^-} $option] } {
+				puts "ERROR: Unknown option '$option' specified.\n"
+				return 1
+			}
+		}}
+	}
+}
+
+# find project file
+set project_file [findProject]
+if {![file exists $project_file]} { return 1 }
+
+open_project $project_file
+
+set runs [list "synth_1" "impl_1"]
+# find runs
+if {[string equal [get_runs -quiet $runs] ""]} {
+	puts "ERROR: Runs '$runs' not found.\n"
+	return 1
+}
+
+foreach run $runs { reset_run $run }
+
+puts "Generating bitstream"
+launch_runs $runs -jobs $jobs -quiet
+
+set last_run [lindex $runs [llength $runs]-1]
+while { [get_property PROGRESS [get_runs $last_run]] != "100%" } {
+	set error 0
+
+	# print status
+	foreach run [get_runs] status [get_property STATUS [get_runs]] progress [get_property PROGRESS [get_runs]] {
+		puts "$progress\t - $run\t - $status"
+		if {[regexp "ERROR" $status]} { set error 1 }
+	}
+
+	if { $error } { return 1}
+	wait_on_run $last_run -timeout 1
+}
+
+# export bitstream
+open_impl_design $last_run
+write_bitstream -force $target
+close_project
+exit 0
