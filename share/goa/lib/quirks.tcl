60ef785cefedaa935a118addd34c7e2fe311f080
Pirmin Duss <pirmin.duss@gapfruit.com>
2023-01-09 14:20:03 +0100

    Support building of libraries
    Issue #27

--- share/goa/lib/quirks.tcl
+++ share/goa/lib/quirks.tcl
@@ -65,17 +65,17 @@ if {[using_api posix]} {
 	# Genode's fork mechanism relies on a specific order of the linked
 	# shared libraries libc, vfs, libm, and posix. Since the order of apis
 	# listed in the 'used_apis' file can be arbitrary, we ensure the
-	# link oder by reordering 'ldlibs'.
+	# link oder by reordering 'ldlibs_exe'.
 	#
 
 	# remove critical entries
 	foreach lib [list libc libm posix] {
-		set idx [lsearch -exact $ldlibs "-l:$lib.lib.so"]
-		if {$idx != -1} { set ldlibs [lreplace $ldlibs $idx $idx] }
+		set idx [lsearch -exact $ldlibs_exe "-l:$lib.lib.so"]
+		if {$idx != -1} { set ldlibs_exe [lreplace $ldlibs_exe $idx $idx] }
 	}
 
 	# prepend known-good link order of the critical libaries
-    set ldlibs [linsert $ldlibs 0 "-l:libc.lib.so" "-l:libm.lib.so" "-l:posix.lib.so"]
+    set ldlibs_exe [linsert $ldlibs_exe 0 "-l:libc.lib.so" "-l:libm.lib.so" "-l:posix.lib.so"]
 }
 
 if {[using_api blit]} {
