c30e76c5c172cb9b9aca6b0699c0817d1c3cdbfb
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2023-09-28 11:25:44 +0200

    cmake: define library that contains dl interface
    'dlopen' and friends are found in libc on Genode.

--- share/goa/cmake/Modules/Platform/Genode.cmake
+++ share/goa/cmake/Modules/Platform/Genode.cmake
@@ -3,3 +3,6 @@ set(GENODE 1)
 set(CMAKE_FIND_LIBRARY_SUFFIXES ".lib.so")
 set(CMAKE_FIND_LIBRARY_PREFIXES "")
 set(CMAKE_SHARED_LIBRARY_SUFFIX ".lib.so")
+
+# library that contains 'dlopen' and friends
+set(CMAKE_DL_LIBS "-l:libc.lib.so")
