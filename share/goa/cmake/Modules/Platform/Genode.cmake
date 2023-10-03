fdd812905ca3802c0525ef3b7871124c174c7228
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2023-10-03 22:23:11 +0200

    cmake: allow '-rpath-link' on Genode
    This helps with finding libraries during linking.

--- share/goa/cmake/Modules/Platform/Genode.cmake
+++ share/goa/cmake/Modules/Platform/Genode.cmake
@@ -3,6 +3,7 @@ set(GENODE 1)
 set(CMAKE_FIND_LIBRARY_SUFFIXES ".lib.so")
 set(CMAKE_FIND_LIBRARY_PREFIXES "")
 set(CMAKE_SHARED_LIBRARY_SUFFIX ".lib.so")
+set(CMAKE_SHARED_LIBRARY_RPATH_LINK_C_FLAG "-Wl,-rpath-link,")
 
 # library that contains 'dlopen' and friends
 set(CMAKE_DL_LIBS "-l:libc.lib.so")
