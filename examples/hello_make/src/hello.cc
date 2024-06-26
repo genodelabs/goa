2fb005e9040235ea0eaff951527596e86bc535ad
Johannes Schlatow <johannes.schlatow@genode-labs.com>
2024-06-26 16:23:37 +0200

    TMP hello_make debug test

--- examples/hello_make/src/hello.cc
+++ examples/hello_make/src/hello.cc
@@ -1,7 +1,15 @@
 #include <base/log.h>
 #include <base/component.h>
 
+extern "C" void wait_for_continue();
+
 void Component::construct(Genode::Env &)
 {
 	Genode::log("Hello");
+
+	wait_for_continue();
+
+	for (int i=0; i < 10; i++) {
+		Genode::log("World!");
+	}
 }
