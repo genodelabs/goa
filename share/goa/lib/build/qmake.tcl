e27f49dc111eb4988af2bc73cd086ead8bf35740
Christian Prochaska <christian.prochaska@genode-labs.com>
2024-10-11 09:06:27 +0200

    Add Qt6 support
    Fixes #106

--- share/goa/lib/build/qmake.tcl
+++ share/goa/lib/build/qmake.tcl
@@ -5,7 +5,16 @@ proc create_or_update_build_dir { } {
 	global cppflags cxxflags ldflags ldflags_so ldlibs_common
 	global ldlibs_exe ldlibs_so project_name env
 
-	set qt5_tool_dir "/usr/local/genode/tool/23.05/bin"
+	if { [regexp qt5_base [used_apis]] } {
+		set qt_version qt5
+		set qt_tool_dir "/usr/local/genode/tool/23.05"
+	} elseif { [regexp qt6_base [used_apis]] } {
+		set qt_version qt6
+		set qt_tool_dir "/usr/local/genode/tool/23.05/qt6"
+	} else {
+		exit_with_error "build via qmake failed: unable to detect Qt version\n" \
+		                "\n Please add qt5_base or qt6_base to your 'used_apis' file."
+	}
 
 	if {$arch == "x86_64"} {
 		set qmake_platform "genode-x86_64-g++"
@@ -24,31 +33,36 @@ proc create_or_update_build_dir { } {
 	file delete -force qmake_root
 	file mkdir qmake_root
 
-	set qt5_api "qt5_base"
+	set qt_api ${qt_version}_base
 
-	file link -symbolic qmake_root/bin $qt5_tool_dir
-	file link -symbolic qmake_root/include [file join [api_archive_dir $qt5_api] include]
+	file link -symbolic qmake_root/bin $qt_tool_dir/bin
+	file link -symbolic qmake_root/include [file join [api_archive_dir $qt_api] include]
 	file link -symbolic qmake_root/lib $abi_dir
-
+	file link -symbolic qmake_root/libexec $qt_tool_dir/libexec
 
 	file mkdir qmake_root/mkspecs
-	file link -symbolic qmake_root/mkspecs/common            [file join [api_archive_dir $qt5_api] mkspecs common]
-	file link -symbolic qmake_root/mkspecs/features          [file join [api_archive_dir $qt5_api] mkspecs features]
-	file link -symbolic qmake_root/mkspecs/$qmake_platform   [file join [api_archive_dir $qt5_api] mkspecs $qmake_platform]
-	file link -symbolic qmake_root/mkspecs/linux-g++         [file join [api_archive_dir $qt5_api] mkspecs linux-g++]
-	file link -symbolic qmake_root/mkspecs/modules           [file join [api_archive_dir $qt5_api] mkspecs modules]
+	file link -symbolic qmake_root/mkspecs/common            [file join [api_archive_dir $qt_api] mkspecs common]
+	file link -symbolic qmake_root/mkspecs/features          [file join [api_archive_dir $qt_api] mkspecs features]
+	file link -symbolic qmake_root/mkspecs/$qmake_platform   [file join [api_archive_dir $qt_api] mkspecs $qmake_platform]
+	file link -symbolic qmake_root/mkspecs/linux-g++         [file join [api_archive_dir $qt_api] mkspecs linux-g++]
+	file link -symbolic qmake_root/mkspecs/modules           [file join [api_archive_dir $qt_api] mkspecs modules]
 	file link -symbolic qmake_root/mkspecs/qconfig.pri       $qmake_platform/qconfig.pri
 	file link -symbolic qmake_root/mkspecs/qmodule.pri       $qmake_platform/qmodule.pri
 
-	set qmake_cflags     "$cppflags $cxxflags "
+	set qmake_cflags    "$cppflags $cxxflags "
 	append qmake_cflags "-D__GENODE__ -D__FreeBSD__=12 "
-	append qmake_cflags "-I$build_dir/qmake_root/include/QtCore/spec/$qmake_platform"
+
+	if { $qt_version == "qt5" } {
+		append qmake_cflags "-I$build_dir/qmake_root/include/QtCore/spec/$qmake_platform"
+	} else {
+		append qmake_cflags "-I$build_dir/qmake_root/mkspecs/$qmake_platform"
+	}
 
 	set qmake_ldlibs { }
 	lappend qmake_ldlibs -l:libc.lib.so
 	lappend qmake_ldlibs -l:libm.lib.so
 	lappend qmake_ldlibs -l:stdcxx.lib.so
-	lappend qmake_ldlibs -l:qt5_component.lib.so
+	lappend qmake_ldlibs -l:${qt_version}_component.lib.so
 
 	set ::env(GENODE_QMAKE_CC)           "${cross_dev_prefix}gcc"
 	set ::env(GENODE_QMAKE_CXX)          "${cross_dev_prefix}g++"
@@ -69,7 +83,7 @@ proc create_or_update_build_dir { } {
 	set ::env(GENODE_QMAKE_LIBS) "-lgcc"
 
 	set qt_conf "qmake_root/mkspecs/$qmake_platform/qt.conf"
-	set cmd [list [file join $qt5_tool_dir qmake] -qtconf $qt_conf [file join $project_dir src]]
+	set cmd [list [file join $qt_tool_dir/bin qmake] -qtconf $qt_conf [file join $project_dir src]]
 
 	diag "create build directory via command:" {*}$cmd
 
